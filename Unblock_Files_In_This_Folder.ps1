210ddd3177692aa9addd915813e0b541639f7c40
Bobby Kearan <bkearan@yahoo.com>
2023-09-13 12:31:20 -0500

    Add files via upload

---
+++ Unblock_Files_In_This_Folder.ps1
@@ -0,0 +1,23 @@
+﻿$folder = $PSScriptRoot
+$SubDirectories = Get-ChildItem -Path $folder -Directory -Recurse
+
+Foreach($sFolder in $SubDirectories){
+$MSIfiles = Get-ChildItem -Path $folder -Filter "*.msi"
+$EXEfiles = Get-ChildItem -Path $folder -Filter "*.exe"
+$ZIPfiles = Get-ChildItem -Path $folder -Filter "*.zip"
+
+Foreach($MSI in $MSIfiles){
+    Write-Host $MSI.FullName
+    Unblock-File $MSI.FullName
+}
+
+Foreach($EXE in $EXEfiles){
+    Write-Host $EXE.FullName
+    Unblock-File $EXE.FullName
+}
+
+Foreach($ZIP in $ZIPfiles){
+    Write-Host $ZIP.FullName
+    Unblock-File $ZIP.FullName
+}
+}
